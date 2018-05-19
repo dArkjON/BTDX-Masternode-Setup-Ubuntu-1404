f4950531d0d3d478463abe457b062b1f6a955e28
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-19 12:14:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,9 +11,10 @@ echo bitcloud:${BTDXPWD} | chpasswd
 #
 # Set rpcuser, rpcpassword and masternode genkey
 #
-printf "** Set rpcuser, rpcpassword and masternode genkey ***"
-mkdir -p /home/bitcloud/.bitcloud
-chown -R bitcloud:bitcloud /home/bitcloud
+printf "** Set rpcuser, rpcpassword and masternode genkey ***\n"
+mkdir -p /home/bitcloud/.bitcloud/
+chown -R bitcloud:bitcloud /home/bitcloud/
+chown bitcloud:bitcloud /tmp/bitcloud.conf
 sudo -u bitcloud cp /tmp/bitcloud.conf /home/bitcloud/.bitcloud/
 sed -i "s/^\(rpcuser=\).*/\rpcuser=btdxmasternode${BTDXPWD}/" /home/bitcloud/.bitcloud/bitcloud.conf
 sed -i "s/^\(rpcpassword=\).*/\rpcpassword=${BTDXPWD}/" /home/bitcloud/.bitcloud/bitcloud.conf
@@ -22,7 +23,7 @@ sed -i "s/^\(masternodeprivkey=\).*/\masternodeprivkey=${MN_KEY}/" /home/bitclou
 #
 # Downloading bootstrap file
 #
-printf "** Downloading bootstrap file ***"
+printf "** Downloading bootstrap file ***\n"
 cd /home/bitcloud/.bitcloud/
 if [ ! -d /home/bitcloud/.bitcloud/blocks ] && [ "$(curl -Is https://bit-cloud.info/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
         sudo -u bitcloud wget  https://bit-cloud.info/${BOOTSTRAP}; \
@@ -34,5 +35,5 @@ fi
 # Starting Bitcloud Service
 #
 # Hint: docker not supported systemd, use of supervisord
-printf "*** Starting Bitcloud Service ***"
+printf "*** Starting Bitcloud Service ***\n"
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
