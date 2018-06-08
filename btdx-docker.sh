c12fee51201b3ea898db92c80039ff98fe035225
David Bergen <david.bergen@gmx.net>
2018-06-08 17:11:23 +0200

    Add dealing with running docker container

--- btdx-docker.sh
+++ btdx-docker.sh
@@ -3,6 +3,7 @@ set -u
 
 DOCKER_REPO="dalijolijo"
 CONFIG="/home/bitcloud/.bitcloud/bitcloud.conf"
+CONTAINER_NAME="btdx-masternode"
 DEFAULT_PORT="8329"
 RPC_PORT="8330"
 TOR_PORT="9050"
@@ -14,12 +15,12 @@ clear
 REUSE="No"
 printf "\nDOCKER SETUP FOR BITCLOUD (BTDX) RPC SERVER\n"
 printf "\nSetup Config file"
-printf "\n-----------------\n"
+printf "\n-----------------"
 if [ -f "$CONFIG" ]
 then
-        printf "Found $CONFIG on your system.\n"
+        printf "\nFound $CONFIG on your system.\n"
         printf "\nDo you want to re-use this existing config file?\n" 
-        printf "Enter Yes or No and Hit [ENTER]: "
+        printf "Enter [Y]es or [N]o and Hit [ENTER]: "
         read REUSE
 fi
 
@@ -83,13 +84,13 @@ if [[ $OS =~ "Ubuntu" ]] || [[ $OS =~ "ubuntu" ]] || [[ $OS =~ "Debian" ]] || [[
         printf "Missing firewall (ufw) on your system.\n"
         printf "Automated firewall setup will open the following ports: 22, ${DEFAULT_PORT}, ${RPC_PORT} and ${TOR_PORT}\n"
         printf "\nDo you want to install firewall (ufw) and execute automated firewall setup?\n"
-        printf "Enter Yes or No and Hit [ENTER]: "
+        printf "Enter [Y]es or [N]o and Hit [ENTER]: "
         read FIRECONF
     else
         printf "Found firewall ufw on your system.\n"
         printf "Automated firewall setup will open the following ports: 22, ${DEFAULT_PORT}, ${RPC_PORT} and ${TOR_PORT}\n"
         printf "\nDo you want to start automated firewall setup?\n"
-        printf "Enter Yes or No and Hit [ENTER]: "
+        printf "Enter [Y]es or [N]o and Hit [ENTER]: "
         read FIRECONF
     fi
 
@@ -135,6 +136,39 @@ fi
 #
 printf "\nStart Docker container"
 printf "\n----------------------\n"
-docker rm btdx-masternode
+sudo docker ps | grep ${CONTAINER_NAME} >/dev/null
+if [ $? -eq 0 ];then
+    printf "Conflict! The container name \'${CONTAINER_NAME}\' is already in use.\n"
+    printf "\nDo you want to stop the running container to start the new one?\n"
+    printf "Enter [Y]es or [N]o and Hit [ENTER]: "
+    read STOP
+
+    if [[ $STOP =~ "Y" ]] || [[ $STOP =~ "y" ]]; then
+        docker stop ${CONTAINER_NAME}
+    else
+	printf "\nDocker Setup Result"
+        printf "\n----------------------\n"
+        printf "Canceled the Docker Setup without starting BitCloud Masternode Docker Container.\n\n"
+	exit 1
+    fi
+fi
+docker rm ${CONTAINER_NAME} >/dev/null
 docker pull ${DOCKER_REPO}/btdx-masternode
-docker run -p ${DEFAULT_PORT}:${DEFAULT_PORT} -p ${RPC_PORT}:${RPC_PORT} -p ${TOR_PORT}:${TOR_PORT} --name btdx-masternode -e BTDXPWD="${BTDXPWD}" -e MN_KEY="${MN_KEY}" -v /home/bitcloud:/home/bitcloud:rw -d ${DOCKER_REPO}/btdx-masternode
+docker run -p ${DEFAULT_PORT}:${DEFAULT_PORT} -p ${RPC_PORT}:${RPC_PORT} -p ${TOR_PORT}:${TOR_PORT} --name ${CONTAINER_NAME} -e BTDXPWD="${BTDXPWD}" -e MN_KEY="${MN_KEY}" -v /home/bitcloud:/home/bitcloud:rw -d ${DOCKER_REPO}/btdx-masternode
+
+#
+# Show result and give user instructions
+#
+clear
+printf "\nDocker Setup Result"
+printf "\n----------------------\n"
+sudo docker ps | grep ${CONTAINER_NAME} >/dev/null
+if [ $? -ne 0 ];then
+    printf "Sorry! Something went wrong. :(\n"
+else
+    printf "GREAT! Your BitCloud Masternode Docker Container is running now! :)\n"
+    printf "\nShow your running docker container \'${CONTAINER_NAME}\' with 'docker ps'\n"
+    sudo docker ps | grep ${CONTAINER_NAME}
+    printf "\nJump inside the docker container with 'docker exec -it ${CONTAINER_NAME} bash'\n"
+    printf "HAVE FUN!\n\n"
+fi
