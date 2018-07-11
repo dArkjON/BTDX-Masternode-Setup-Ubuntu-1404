066e2eb238a099db0a29e29e4793651424292df8
c1xx <c1xx@pm.me>
2018-07-11 13:51:22 +0200

    Added IP-address detection (#10)
    External VPS IP address will be automatically detected, but user have the possibility to enter other IP address

--- btdx-docker.sh
+++ btdx-docker.sh
@@ -9,6 +9,7 @@ RPC_PORT="8330"
 TOR_PORT="9050"
 WEB="bit-cloud.info/files" # without "https://" and without the last "/" (only HTTPS accepted)
 BOOTSTRAP="bootstrap.tar.gz"
+IP=$(curl -s https://bit-cloud.info/showip.php)
 
 #
 # Color definitions
@@ -25,7 +26,7 @@ clear
 REUSE="No"
 printf "\nDOCKER SETUP FOR ${BTDX_COL}BITCLOUD (BTDX)${NO_COL} MASTERNODE SERVER\n"
 printf "\nSetup Config file"
-printf "\n-----------------"
+printf "\n-----------------\n"
 if [ -f "$CONFIG" ]
 then
         printf "\nFound $CONFIG on your system.\n"
@@ -35,14 +36,17 @@ then
 fi
 
 if [[ $REUSE =~ "N" ]] || [[ $REUSE =~ "n" ]]; then
-        printf "\nFound the following IP-addresses on this Server:\n"
-        hostname -I
-	printf "\nEnter the IP-address of your ${BTDX_COL}BitCloud${NO_COL} Masternode VPS and Hit [ENTER]: "
-        read BTDX_IP
+	read -e -p "Is this IP-address $IP your VPS IP-address? [Y/n]: " ipaddress
+	if [[ ("$ipaddress" == "n" || "$ipaddress" == "N") ]]; then
+		printf "\nEnter the IP-address of your ${BTDX_COL}BitCloud${NO_COL} Masternode VPS and Hit [ENTER]: "
+		read BTDX_IP
+	else
+		BTDX_IP=$(echo $IP)
+	fi
         printf "Enter your ${BTDX_COL}BitCloud${NO_COL} Masternode genkey respond and Hit [ENTER]: "
         read MN_KEY
 else
-        source $CONFIG
+	source $CONFIG
         BTDX_IP=$(echo $externalip)
         MN_KEY=$(echo $masternodeprivkey)
 fi
