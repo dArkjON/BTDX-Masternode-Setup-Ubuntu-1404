9cc6a24cde0a598b517f056550a89ad16d63fde3
c1xx <c1xx@pm.me>
2018-09-16 21:33:58 +0200

    Initial upload

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+COIN_CHAIN='https://bitcloud.network/files/blockchain.tar.gz'
+
+mkdir /root/btdx_temp >/dev/null 2>&1
+cd /root/btdx_temp >/dev/null 2>&1
+echo -e "Downloading and extracting BitCloud blockchain files."
+wget -q $COIN_CHAIN
+tar -xzvf blockchain.tar.gz -C /root/btdx_temp >/dev/null 2>&1
+rm -rf /root/.bitcloud/blocks >/dev/null 2>&1
+rm -rf /root/.bitcloud/chainstate >/dev/null 2>&1
+mv /root/btdx_temp/root/Bootstrap/.bitcloud/blocks /root/.bitcloud >/dev/null 2>&1
+mv /root/btdx_temp/root/Bootstrap/.bitcloud/chainstate /root/.bitcloud >/dev/null 2>&1
+cd ~
+rm -R /root/btdx_temp >/dev/null 2>&1
