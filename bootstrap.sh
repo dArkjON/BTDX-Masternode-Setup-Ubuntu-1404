c4ef5663e97d6e7a64a540bd05ac7578e2d56fa7
c1xx <c1xx@pm.me>
2018-11-05 08:47:11 +0100

    fix
    local path fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ wget -q $COIN_CHAIN
 tar -xzvf bitcloud-blockchain.tar.gz -C /root/btdx_temp >/dev/null 2>&1
 rm -rf /root/.bitcloud/blocks >/dev/null 2>&1
 rm -rf /root/.bitcloud/chainstate >/dev/null 2>&1
-mv /root/btdx_temp/root/Bootstrap/.bitcloud/blocks /root/.bitcloud >/dev/null 2>&1
-mv /root/btdx_temp/root/Bootstrap/.bitcloud/chainstate /root/.bitcloud >/dev/null 2>&1
+mv /root/btdx_temp/root/.bitcloud/blocks /root/.bitcloud >/dev/null 2>&1
+mv /root/btdx_temp/root/.bitcloud/chainstate /root/.bitcloud >/dev/null 2>&1
 cd ~
 rm -R /root/btdx_temp >/dev/null 2>&1
