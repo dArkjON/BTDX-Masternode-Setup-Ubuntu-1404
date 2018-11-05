643bc941a6366265ae52b6e529dabfc339cdd900
c1xx <c1xx@pm.me>
2018-11-05 08:49:02 +0100

    fix
    Bootstrap fix

--- masternode-setup.sh
+++ masternode-setup.sh
@@ -241,11 +241,11 @@ mkdir /root/btdx_temp >/dev/null 2>&1
 cd /root/btdx_temp >/dev/null 2>&1
 echo -e "Downloading and extracting BitCloud blockchain files."
 wget -q $COIN_CHAIN
-tar -xzvf blockchain.tar.gz -C /root/btdx_temp >/dev/null 2>&1
+tar -xzvf bitcloud-blockchain.tar.gz -C /root/btdx_temp >/dev/null 2>&1
 rm -rf /root/.bitcloud/blocks >/dev/null 2>&1
 rm -rf /root/.bitcloud/chainstate >/dev/null 2>&1
-mv /root/btdx_temp/root/Bootstrap/.bitcloud/blocks /root/.bitcloud >/dev/null 2>&1
-mv /root/btdx_temp/root/Bootstrap/.bitcloud/chainstate /root/.bitcloud >/dev/null 2>&1
+mv /root/btdx_temp/root/.bitcloud/blocks /root/.bitcloud >/dev/null 2>&1
+mv /root/btdx_temp/root/.bitcloud/chainstate /root/.bitcloud >/dev/null 2>&1
 cd ~
 rm -R /root/btdx_temp >/dev/null 2>&1
 }
