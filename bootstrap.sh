5d2c05387d658760ca818bdbc4605f222af02af4
c1xx <c1xx@pm.me>
2018-11-05 08:35:58 +0100

    fix
    file name fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ mkdir /root/btdx_temp >/dev/null 2>&1
 cd /root/btdx_temp >/dev/null 2>&1
 echo -e "Downloading and extracting BitCloud blockchain files."
 wget -q $COIN_CHAIN
-tar -xzvf blockchain.tar.gz -C /root/btdx_temp >/dev/null 2>&1
+tar -xzvf bitcloud-blockchain.tar.gz -C /root/btdx_temp >/dev/null 2>&1
 rm -rf /root/.bitcloud/blocks >/dev/null 2>&1
 rm -rf /root/.bitcloud/chainstate >/dev/null 2>&1
 mv /root/btdx_temp/root/Bootstrap/.bitcloud/blocks /root/.bitcloud >/dev/null 2>&1
